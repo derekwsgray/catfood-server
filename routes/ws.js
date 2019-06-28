12f4fd62f38f57930d02c5138f767a58247226e9
Derek Gray <dgray@uncharted.software>
2019-06-28 19:26:21 -0400

    Add preliminary cron-job support.

--- routes/ws.js
+++ routes/ws.js
@@ -1,7 +1,10 @@
+const getCronString = require('@darkeyedevelopers/natural-cron.js');
+const schedule = require('node-schedule');
 const logger = console;
 
 const PULSE_WIDTH_OPEN = 800;
 const PULSE_WIDTH_CLOSED = 2050;
+const DURATION_OPEN = 1000;
 
 function promiseTimeout(time) {
     return new Promise(function(resolve, reject) {
@@ -15,6 +18,8 @@ class WebsocketHandler {
 
     constructor() {
         this.motor = null;
+        this.nextJobId = 1;
+        this.jobMap = {};
     }
 
     init(GpioConstructor, socketIO) {
@@ -29,34 +34,20 @@ class WebsocketHandler {
             socket.on('operation', (from, data) => this.onOperation(socket, from, data));
 
             setInterval(function () {
-                socket.emit('server-status', { timestamp: new Date().toString() });
+                socket.emit('server-status', { timestamp: new Date().toLocaleString() });
             }, 5000);
 
         });
     }
 
-    onOperation(socket, data) {
+    async onOperation(socket, data) {
         logger.info(`Operation ${data.operation}`);
 
         if (data.operation === 'feed-now') {
-            socket.emit('operation-status', {
-                operation: 'feed-now',
-                status: 'Feeding...'
-            });
+            await this.feedNow(socket, DURATION_OPEN);
 
-            this.feedNow(data.duration).then(() => {
-                return Promise.resolve(); //this.settle();
-            }).then(() => {
-                socket.emit('operation-status', {
-                    operation: 'feed-now',
-                    status: 'Feeding Done!'
-                });
-            }).catch((e) => {
-                socket.emit('operation-status', {
-                    operation: 'feed-now',
-                    status: 'Feeding Problem!: ' + e
-                });
-            });
+        } else if (data.operation === 'cron-job') {
+            await this.scheduleCronJob(socket, data.english);
 
         } else {
             socket.emit('operation-status', {
@@ -66,18 +57,100 @@ class WebsocketHandler {
         }
     }
 
-    feedNow(duration) {
-        return new Promise((resolve, reject) => {
-            try {
-                this.motor.servoWrite(PULSE_WIDTH_OPEN);
-                setTimeout(() => {
-                    this.motor.servoWrite(PULSE_WIDTH_CLOSED);
-                    resolve();
-                }, duration);
-            } catch(e) {
-                reject(e);
-            }
+    async feedNow(socket, duration, silent = false) {
+        socket.emit('operation-status', {
+            operation: 'feed-now',
+            status: 'Feeding...',
+            timestamp: new Date().toLocaleString()
         });
+
+        try {
+            this.motor.servoWrite(PULSE_WIDTH_OPEN);
+            await promiseTimeout(duration);
+            this.motor.servoWrite(PULSE_WIDTH_CLOSED);
+
+            if (!silent) {
+                socket.emit('operation-status', {
+                    operation: 'feed-now',
+                    status: 'Feeding Done!',
+                    timestamp: new Date().toLocaleString(),
+                    complete: true
+                });
+            }
+        } catch(e) {
+            socket.emit('operation-status', {
+                operation: 'feed-now',
+                status: 'Feeding Problem!: ' + e,
+                timestamp: new Date().toLocaleString(),
+                error: true
+            });
+        }
+    }
+
+    /**
+     * The cron format consists of:
+     *
+     *  *    *    *    *    *    *
+     *  ┬    ┬    ┬    ┬    ┬    ┬
+     *  │    │    │    │    │    │
+     *  │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+     *  │    │    │    │    └───── month (1 - 12)
+     *  │    │    │    └────────── day of month (1 - 31)
+     *  │    │    └─────────────── hour (0 - 23)
+     *  │    └──────────────────── minute (0 - 59)
+     *  └───────────────────────── second (0 - 59, OPTIONAL)
+     * @param socket
+     * @param english
+     */
+    scheduleCronJob(socket, english) {
+        socket.emit('operation-status', {
+            operation: 'cron-job',
+            status: 'Scheduling...'
+            timestamp: new Date().toLocaleString(),
+        });
+
+        try {
+            let cron = getCronString(english).replace('?', '*'); // the parser doesn't understand '?'.
+
+            // Doesn't support seconds. so...
+            //if (cron.split(' ').length < 6) {
+                cron = cron.substring(0, cron.lastIndexOf(' '));
+            //}
+
+            logger.info('Cron Job Translated to: ' + cron);
+
+            const job = schedule.scheduleJob(cron, async () => {
+
+                await this.feedNow(socket, DURATION_OPEN, true);
+
+                socket.emit('operation-status', {
+                    operation: 'cron-job',
+                    status: 'Feeding done. Next feeding at ' + new Date(job.nextInvocation()).toLocaleString(),
+                    timestamp: new Date().toLocaleString(),
+                    complete: true
+                });
+            });
+            this.jobMap[this.nextJobId++] = job;
+
+            //const nextExecution = cronParser.parseExpression(cron, { tz: 'America/Toronto' }).next().toString();
+
+            socket.emit('operation-status', {
+                operation: 'cron-job',
+                status: 'Scheduled. Next feeding at ' + new Date(job.nextInvocation()).toLocaleString(),
+                timestamp: new Date().toLocaleString(),
+                complete: true
+            });
+
+        } catch(e) {
+            socket.emit('operation-status', {
+                operation: 'cron-job',
+                status: 'Scheduling Problem!: ' + e,
+                timestamp: new Date().toLocaleString(),
+                error: true
+            });
+        }
+
+
     }
 
 }
